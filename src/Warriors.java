e346efda962732d0391a46797daf35e752f8f3b2
Veronika <veronikacasado@icloud.com>
2022-09-23 16:05:36 +0200

    Warriors class

---
+++ src/Warriors.java
@@ -0,0 +1,28 @@
+public class Warriors {
+    private int stamina;
+    private int strength;
+
+    public Warriors(int stamina, int strength) {
+        this.stamina = stamina;
+        this.strength = strength;
+    }
+
+
+
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+}
